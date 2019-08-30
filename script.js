91a1b9c6a2ec5818f2978fcadfe89281c01990e4
Kostya Yelskiy <koortttt@gmail.com>
2019-08-30 18:06:47 +0300

    added some rules and gitignore

--- script.js
+++ script.js
@@ -3,15 +3,13 @@ $.ajax({
     url: 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json',
     dataType: 'json',
     success: function (data) {
-        var exchange_date = (data[0].exchangedate);
-        var rate_value = (data[0].rate).toFixed(2);
-        $('#date').html(exchange_date);
-        $('#price').html('1 USD = ' + rate_value + ' UAH');
+        $('#date').html(data[0].exchangedate);
+        $('#price').html('1 USD = ' + (data[0].rate).toFixed(2) + ' UAH');
         $('#submit').on('click', function () {
-            if ($('#amount').val() >= 1) {
-                $('#result').html((rate_value * $('#amount').val()).toFixed(2) + " UAH");
+            if ($('#amount').val() >= 1 && isNaN($('#amount').val()) !== true) {
+                $('#result').html(((data[0].rate).toFixed(2) * $('#amount').val()).toFixed(2) + " UAH");
             } else {
-                $('#result').html('Enter at least 1 digit!')
+                $('#result').html('Error! There is no digits or letters are here!')
             }
         });
     },
