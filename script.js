995cb22fe228e7562d80f27bd4aa4b84908d29fe
Kostya Yelskiy <koortttt@gmail.com>
2019-08-30 18:09:11 +0300

    minified style, and js. changed one tag name in html

--- script.js
+++ script.js
@@ -1,18 +1,1 @@
-$.ajax({
-    type: 'GET',
-    url: 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json',
-    dataType: 'json',
-    success: function (data) {
-        $('#date').html(data[0].exchangedate);
-        $('#price').html('1 USD = ' + (data[0].rate).toFixed(2) + ' UAH');
-        $('#submit').on('click', function () {
-            if ($('#amount').val() >= 1 && isNaN($('#amount').val()) !== true) {
-                $('#result').html(((data[0].rate).toFixed(2) * $('#amount').val()).toFixed(2) + " UAH");
-            } else {
-                $('#result').html('Error! There is no digits or letters are here!')
-            }
-        });
-    },
-});
-
-
+$.ajax({type:"GET",url:"https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json",dataType:"json",success:function(t){$("#date").html(t[0].exchangedate),$("#price").html("1 USD = "+t[0].rate.toFixed(2)+" UAH"),$("#submit").on("click",function(){$("#amount").val()>=1&&!0!==isNaN($("#amount").val())?$("#result").html((t[0].rate.toFixed(2)*$("#amount").val()).toFixed(2)+" UAH"):$("#result").html("Error! There is no digits or letters are here!")})}});
