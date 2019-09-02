bfda2a8869c830a75bb9f7f595251f7a7e77617f
Kostya Yelskiy <koortttt@gmail.com>
2019-09-02 08:30:08 +0300

    added new format of taking data from api, added some rules

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-$.ajax({type:"GET",url:"https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json",dataType:"json",success:function(t){$("#date").html(t[0].exchangedate),$("#price").html("1 USD = "+t[0].rate.toFixed(2)+" UAH"),$("#submit").on("click",function(){$("#amount").val()>=1&&!0!==isNaN($("#amount").val())?$("#result").html((t[0].rate.toFixed(2)*$("#amount").val()).toFixed(2)+" UAH"):$("#result").html("Error! There is no digits or letters are here!")})}});
+var input = document.getElementById("amount");
+input.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("submit").click();
+    }
+});
+
+function formatDate(date) {
+    var d = new Date(date),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) month = '0' + month;
+    if (day.length < 2) day = '0' + day;
+
+    return [year, month, day].join('');
+}
+
+let dym_format = formatDate(Date());
+let url = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?valcode=USD&date=" + dym_format + "&json";
+
+function dataHandler() {
+    $.ajax({
+        type: "GET",
+        url: url,
+        success: function (t) {
+            if ($('#amount').val() >= 100000) {
+                $("#result").html("You have too big salary. СБУ is watching you :)")
+            }
+            $("#date").html(t[0].exchangedate), $("#price").html("1 USD = " + t[0].rate.toFixed(2) + " UAH"), $("#submit").on("click", function () {
+                $("#amount").val() >= 1 && !0 !== isNaN($("#amount").val()) ? $("#result").html((t[0].rate.toFixed(2) * $("#amount").val()).toFixed(2) + " UAH") : $("#result").html("Error! There is no digits or letters are here!")
+            })
+        }
+    });
+}
+
+dataHandler();
