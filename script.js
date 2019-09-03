a913c1b3d5d4a2d7751a16467304c8366f93324a
Kostya Yelskiy <koortttt@gmail.com>
2019-09-03 10:15:04 +0300

    edited title and cleaned js

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ let dym_format = formatDate(Date()),
 function dataHandler() {
     $.ajax({
         type: "GET", url: url, success: function (t) {
-
             $("#amount").val() >= 1e5 && $("#result").html("You have too big salary. СБУ is watching you :)"), $("#date").html(t[0].exchangedate), $("#price").html("1 USD = " + t[0].rate.toFixed(2) + " UAH"), $("#submit").on("click", function () {
                 $("#amount").val() >= 1 && !0 !== isNaN($("#amount").val()) ? $("#result").html((t[0].rate.toFixed(2) * $("#amount").val()).toFixed(2) + " UAH") : $("#result").html("Error! There is allowed only digits!")
             })
